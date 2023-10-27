779dcfca6f70049cb28817c7c4d769c9178e719a
yungshun317 <seraph317@gmail.com>
2023-10-27 23:05:35 +0800

    [CHORE] Initialize the npm project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<>React Three Fiber Application</>);
