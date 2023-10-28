0b17ed18e56cb7f8da728b549838943a897a1e61
yungshun317 <seraph317@gmail.com>
2023-10-28 09:55:39 +0800

    [FEAT] Add a new mesh using a `PlaneGeometry` and a `meshBasicMaterial`

--- src/Scene.js
+++ src/Scene.js
@@ -4,16 +4,23 @@ import * as THREE from "three";
 
 const Scene = () => {
     const cubeRef = useRef();
-    // const planeRef = useRef();
+    const planeRef = useRef();
     const { gl, camera } = useThree();
+    camera.position.x = 5;
 
     useFrame((state, delta) => {
+        cubeRef.current.rotation.y += delta;
+        planeRef.current.rotation.y += delta;
         state.camera.position.x = Math.sin(state.clock.elapsedTime);
         console.log(state.clock.elapsedTime);
     });
 
     return (
         <>
+            <mesh ref={planeRef} position-x={-2}>
+                <planeGeometry args={[2, 2]} />
+                <meshBasicMaterial color="orange" side={THREE.DoubleSide} />
+            </mesh>
             <mesh ref={cubeRef} position-x={2}>
                 <boxGeometry />
                 <meshBasicMaterial color="purple" />
